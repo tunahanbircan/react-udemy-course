3e3dc4a6c7a90b1173bd2b614be4501a0f8f1cf6
tunahanbircan <tunahanbircan42@gmail.com>
2023-04-18 15:57:23 +0300

    #

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
+  const para = document.createElement('p');
+  para.textContent = 'This is alosu visible';
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
